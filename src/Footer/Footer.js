caf0fc5927a63bbc27af651eac4b73d4388031dd
bestanly <ibestanly@gmail.com>
2019-02-15 16:51:32 -0500

    Complete Desktop version

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,16 +4,20 @@ import logo from "../images/logo.png"
 const Footer = props => {
   return (
     <footer className="footer-div">
-      
       <h3 className="footer-copyright-h3">
-          Copyright © 2019 Pepe Rosso Social
+        Copyright © 2019 Pepe Rosso Social
       </h3>
       <img className="footer-logo-img" src={logo} alt="logo" />
       <div className="footer-social-media-div">
-            <a href="https://www.facebook.com/PepeRossoSocial/"><i className="fab fa-facebook-square" /> </a>
-            <a href="https://twitter.com/peperossosocial"><i className="fab fa-twitter"/></a>
-            <a href="https://www.instagram.com/explore/locations/157793329/"><i class="fab fa-instagram"></i></a>
-
+        <a href="https://www.facebook.com/PepeRossoSocial/">
+            <i className="fab fa-facebook-square fa-2x" />
+        </a>
+        <a href="https://twitter.com/peperossosocial">
+            <i className="fab fa-twitter fa-2x" />
+        </a>
+        <a href="https://www.instagram.com/explore/locations/157793329/">
+            <i class="fab fa-instagram fa-2x" />
+        </a>
       </div>
     </footer>
   );
