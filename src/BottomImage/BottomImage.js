fffad4f59c0699fd2045266e50e94ebcd9c1b44d
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-02-15 16:51:53 -0500

    fixed indentation

--- src/BottomImage/BottomImage.js
+++ src/BottomImage/BottomImage.js
@@ -7,8 +7,15 @@ const BottomImage = () => {
             <div className="BottomImage-imgContainer">
                 <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/8.jpg" alt="Restaurant Mailbox" className="BottomImage-img"/>
             </div>
-            <div className="BottomImage-imgContainer"><img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/14.jpg" alt="Pasta Made Fresh In-house" className="BottomImage-img"/></div>
-            <div className="BottomImage-imgContainer"><img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/ravioli.jpg" alt="Pasta Name" className="BottomImage-img"/></div>
+
+            <div className="BottomImage-imgContainer">
+                <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/14.jpg" alt="Pasta Made Fresh In-house" className="BottomImage-img"/>
+            </div>
+
+            <div className="BottomImage-imgContainer">
+                <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/ravioli.jpg" alt="Pasta Name" className="BottomImage-img"/>
+            </div>
+            
         </div>
     )
 }
