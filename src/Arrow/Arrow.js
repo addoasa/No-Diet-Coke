c81fb998f1220e52b1c7f84379f29fdd0b6f9896
bestanly <ibestanly@gmail.com>
2019-02-16 14:01:20 -0500

    fix indentation

--- src/Arrow/Arrow.js
+++ src/Arrow/Arrow.js
@@ -5,10 +5,9 @@ import { Link } from "react-router-dom";
 const Arrow =(props) => {
     return (
       <div className="arrow-div">
-      <a href="#home">
-        <i class="fas fa-arrow-up fa-4x" /> <br/> Top
-      </a>
-        
+        <a href="#home">
+            <i class="fas fa-arrow-up fa-4x" /> <br/> Top
+        </a>
       </div>
     );
 }
