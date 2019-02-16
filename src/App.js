071b01906b98a1354a67f2cb6e3d8c2805616d2d
addoasa <addoasa94@gmail.com>
2019-02-16 07:06:25 -0500

    Removed whole menu from app.js and created a MainMenu component for it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
-//{/* ---------------------------------------------------------------------- */}
 import './App.css';
-// we can either work out of one css file (./App.css) and do as danny said ( label our classNames as className = "Dessert_navbar" or className = "Contact_image")  or we can do separate css files for each component. ¯\_(ツ)_/¯
-
-// {/* ---------------------------------------------------------------------- */}
-
 import Header from "./Header/Header"
 import TopImage from "./TopImage/TopImage"
 import MainMenu from "./MainMenu/MainMenu"
