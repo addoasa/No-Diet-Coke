eb48d761d9a71cbb66577c5b55ddf40e6ba10350
CodeandCruz <cruzjosenyc@gmail.com>
2019-02-17 01:06:22 -0500

    Added events section, no styling yet. Will work on that. Also will map through, a json file for the drinks

--- src/Cantina/Events/Events.js
+++ src/Cantina/Events/Events.js
@@ -3,8 +3,28 @@ import "./Events.css"
 
 const Events = () => {
   return(
-    <div>
-      <h1>This is Events</h1>
+    <div className="Events-container ">
+      <div className="Events-info-container">
+        <h1 className="Events-title">WEEKLY EVENTS</h1>
+        <br/>
+        <br/>
+        <h2 className="Events-section">HAPPY HOUR</h2>
+        <br/>
+        <p className="Events-info">Monday to Friday, 5:00pm-7:00pm</p>
+        <p className="Events-info">Imported Beer $4</p>
+        <p className="Events-info">House Wine $4/glass</p>
+        <br/>
+        <p className="Events-section">SPARKLING BRUNCH</p>
+        <br/>
+        <p className="Events-info">Saturday and Sunday</p>
+        <br/>
+        <p className="Events-section">PREMIER LEAGUE ACTION</p>
+        <br/>
+        <p className="Events-info">Every Saturday</p>
+      </div>
+      <div className="Events-image-wrapper">
+        <img className="Events-image" src="http://www.peperossosocial.com/wp-content/uploads/2014/09/81.jpg"/>
+      </div>
     </div>
   )
 }
