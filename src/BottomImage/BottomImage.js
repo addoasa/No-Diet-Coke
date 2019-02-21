68fe9ca6a2bbba981ffff7d6e1036db1eb3dfd4f
addoasa <addoasa94@gmail.com>
2019-02-21 13:55:06 -0500

    More Responsive behavior

--- src/BottomImage/BottomImage.js
+++ src/BottomImage/BottomImage.js
@@ -4,17 +4,12 @@ import "./BottomImage.css"
 const BottomImage = () => {
     return(
         <div className="BottomImage-container">
-            <div className="BottomImage-imgContainer">
                 <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/8.jpg" alt="Restaurant Mailbox" className="BottomImage-img"/>
-            </div>
 
-            <div className="BottomImage-imgContainer">
                 <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/14.jpg" alt="Pasta Made Fresh In-house" className="BottomImage-img"/>
-            </div>
 
-            <div className="BottomImage-imgContainer">
+
                 <img src="http://www.peperossosocial.com/wp-content/uploads/2014/09/ravioli.jpg" alt="Pasta Name" className="BottomImage-img"/>
-            </div>
             
         </div>
     )
